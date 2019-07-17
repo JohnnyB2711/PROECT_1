e2b9c3a25d441b3b3a3fcdc10f85f649ad83b515
Евгения <eugenebeklemisheva2711@gmail.com>
2019-07-17 02:33:43 -0700

    функция js по контролю ввода только цифр (вариант 1)

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+    gulp.src('app/sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+
+});
