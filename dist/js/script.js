bf13321d154ae9251d57c1e2a990106c5b4a378d
Евгения <eugenebeklemisheva2711@gmail.com>
2019-07-25 04:27:52 -0700

    создание функции проверки полей(2)

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     var val_email = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
     var val_name = /^[a-zа-яё]*$/i;
     var val_password = /[0-9a-zA-Z!@#$%^&*]{6,}/;
+    var val_spassword = /[$('#password')]/ig;
 
     var name = $('#name');
     var username = $('#username');
@@ -12,73 +13,48 @@ $(document).ready(function () {
     var button = $('input[type="submit"]');
     var form = $('#form_reg');
     var checkbox = $('input[name="checkbox-test"]');
-    var mas = [ name , username , email , password , second_password ];
+    var mas = [name, username, email, password, second_password];
 
     /*Функция идентификации ошибок*/
-    function Message(text){
-        return ("<span class=\"invalid\">")+ text +("<img src=\"img/invalid.png\"></span>");
+    function Message(text) {
+        return ("<span class=\"invalid\">") + text + ("<img src=\"img/invalid.png\"></span>");
     }
-    function Color_border(isinvalid,element){
-        if (isinvalid){
+
+    function Color_border(isinvalid, element) {
+        if (isinvalid) {
             element.css("border-bottom", "2px solid #c80000");
-        }
-        else{
+        } else {
             element.css("border-bottom", "2px solid #ccc");
         }
     };
-    function Proverca_poley(element,text,validation,k){
-        element.blur(function () {
-            element.siblings().remove();
-            if (element.val() != '') {
-                if (element.val().search(validation) == 0) {
-                    Color_border(false ,element);
-                } else {
-                    Color_border(true , element);
-                    element.after(Message(text));
-                }
-            } else {
-                Color_border(true , element);
-                element.after(Message(text));
-            }
-        });
+
+    function Proverca_poley(element, text, isinvalid, k) {
+        name.siblings().remove();
+        if (isinvalid) {
+            Color_border(false, element);
+        } else {
+            Color_border(true, element);
+            element.after(Message(text));
+        }
     };
     /*Поле NAME*/
-    (Proverca_poley (name,'Name is required',val_name));
-   /* name.blur(function () {
-        text='Name is required';
-        name.siblings().remove();
-        if (name.val() != '') {
-            if (name.val().search(val_name) == 0) {
-                Color_border(false , name);
-            } else {
-                Color_border(true , name);
-                name.after(Message(text));
-            }
-        } else {
-            Color_border(true , name);
-            name.after(Message(text));
+    name.blur(function () {
+        if (name.val() == '') {
+            Proverca_poley(name, 'Name is required', false);
+            return
         }
-    });*/;
+        Proverca_poley(name, 'Введите только буквы!', name.val().search(val_name) == 0);
+    });
     /*Поле USERNAME*/
-    (Proverca_poley(username,'User name is required',val_name));
-    /*
     username.blur(function () {
-        text='User name is required';
-        username.siblings().remove();
-        if (username.val() != '') {
-            if (username.val().search(val_name) == 0) {
-                Color_border(false , username);
-            } else {
-                Color_border(true , username);
-                username.after(Message(text));
-            }
-        } else {
-            Color_border(true , username);
-            username.after(Message(text));
+        if (username.val() == '') {
+            Proverca_poley(username, 'User name is required', false);
+            return
         }
-    });*/;
+        Proverca_poley(username, 'Введите только буквы!', username.val().search(val_name) == 0);
+    });
     /*Поле EMAIL*/
-    Proverca_poley(email,'Valid email is required',val_email);
+    Proverca_poley(email, 'Valid email is required', val_email);
     /*
     email.blur(function () {
         text='Valid email is required';
@@ -96,9 +72,10 @@ $(document).ready(function () {
             email.after(Message(text));
             ;
         }
-    });*/;
+    });*/
+    ;
     /*Поле PASSWORD*/
-    Proverca_poley(password,'Password is required',val_password);
+    Proverca_poley(password, 'Password is required', val_password);
     /*password.blur(function () {
         password.siblings().remove();
         if (password.val().length > 5) {
@@ -107,17 +84,16 @@ $(document).ready(function () {
             proverca(true , email);
             Color_border.after(Message(text));
         }
-    });*/;
+    });*/
+    ;
     /*Поле SECONDPASSWORD*/
-
-    //Proverca_poley(second_password,'Passwords don\'t match',password.val());
-
-    second_password.blur(function () {
+    Proverca_poley(second_password, 'Passwords don\'t match', val_spassword);
+    /*second_password.blur(function () {
         text='Passwords don\'t match';
        second_password.siblings().remove();
         if (second_password.val() != '') {
-            if (password.val() == second_password.val()) {
-            //if (second_password.val().search(password.val())==0) {
+            //if (password.val() == second_password.val()) {
+            if (second_password.val().search(password.val())==0) {
                 Color_border(false , second_password);
             }
             else{
@@ -130,9 +106,12 @@ $(document).ready(function () {
         }
     })
     /*ЧЕКБОКС*/
-    checkbox.click(function() {
+    ;
+
+
+    checkbox.click(function () {
         button.siblings('.invalid').remove();
-        if(!((checkbox).is(':checked'))){
+        if (!((checkbox).is(':checked'))) {
             button.after(Message('You mast accept terms and conditions'));
         }
     });
@@ -149,4 +128,5 @@ $(document).ready(function () {
             event.preventDefault();
         }
     });
-});
+})
+;
