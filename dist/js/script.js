1ddba935eb3f4c301a6b5aa963816383add69212
Евгения <eugenebeklemisheva2711@gmail.com>
2019-07-25 03:35:44 -0700

    создание функции проверки полей

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     var val_email = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
     var val_name = /^[a-zа-яё]*$/i;
+    var val_password = /[0-9a-zA-Z!@#$%^&*]{6,}/;
 
     var name = $('#name');
     var username = $('#username');
@@ -9,93 +10,140 @@ $(document).ready(function () {
     var password = $('#password');
     var second_password = $('#second_password');
     var button = $('input[type="submit"]');
-    var accept = $('.accept');
     var form = $('#form_reg');
+    var checkbox = $('input[name="checkbox-test"]');
     var mas = [ name , username , email , password , second_password ];
-    /*Поле NAME*/
-    name.blur(function () {
-        $("#invalid_name").remove();
-        if (name.val() != '') {
-            ;
-            if (name.val().search(val_name) == 0) {
-                name.css("border-bottom", "2px solid #ccc");
+
+    /*Функция идентификации ошибок*/
+    function Message(text){
+        return ("<span class=\"invalid\">")+ text +("<img src=\"img/invalid.png\"></span>");
+    }
+    function Color_border(isinvalid,element){
+        if (isinvalid){
+            element.css("border-bottom", "2px solid #c80000");
+        }
+        else{
+            element.css("border-bottom", "2px solid #ccc");
+        }
+    };
+    function Proverca_poley(element,text,validation,k){
+        element.blur(function () {
+            element.siblings().remove();
+            if (element.val() != '') {
+                if (element.val().search(validation) == 0) {
+                    Color_border(false ,element);
+                } else {
+                    Color_border(true , element);
+                    element.after(Message(text));
+                }
             } else {
-                name.css("border-bottom", "2px solid #c80000");
-                name.after('<span class="invalid" id="invalid_name">Name is required<img src="img/invalid.png"></span>');
+                Color_border(true , element);
+                element.after(Message(text));
+            }
+        });
+    };
+    /*Поле NAME*/
+    (Proverca_poley (name,'Name is required',val_name));
+   /* name.blur(function () {
+        text='Name is required';
+        name.siblings().remove();
+        if (name.val() != '') {
+            if (name.val().search(val_name) == 0) {
+                Color_border(false , name);
+            } else {
+                Color_border(true , name);
+                name.after(Message(text));
             }
         } else {
-            name.css("border-bottom", "2px solid #c80000");
-            name.after('<span class="invalid" id="invalid_name">Name is required<img src="img/invalid.png"></span>');
+            Color_border(true , name);
+            name.after(Message(text));
         }
-    });
+    });*/;
     /*Поле USERNAME*/
+    (Proverca_poley(username,'User name is required',val_name));
+    /*
     username.blur(function () {
-        $("#invalid_username").remove();
+        text='User name is required';
+        username.siblings().remove();
         if (username.val() != '') {
             if (username.val().search(val_name) == 0) {
-                username.css("border-bottom", "2px solid #ccc");
+                Color_border(false , username);
             } else {
-                username.css("border-bottom", "2px solid #c80000");
-                username.after('<span class="invalid" id="invalid_username">User name is required<img src="img/invalid.png"></span>');
+                Color_border(true , username);
+                username.after(Message(text));
             }
         } else {
-            username.css("border-bottom", "2px solid #c80000");
-            username.after('<span class="invalid" id="invalid_username">User name is required<img src="img/invalid.png"></span>');
+            Color_border(true , username);
+            username.after(Message(text));
         }
-    });
+    });*/;
     /*Поле EMAIL*/
+    Proverca_poley(email,'Valid email is required',val_email);
+    /*
     email.blur(function () {
-        $("#invalid_email").remove();
+        text='Valid email is required';
+        email.siblings().remove();
         if (email.val() != '') {
-            /*ПРОВЕРКА EMAIL*/
             if (email.val().search(val_email) == 0) {
-                email.css("border-bottom", "2px solid #ccc");
+                Color_border(false , email);
             } else {
-                email.css("border-bottom", "2px solid #c80000");
-                email.after('<span class="invalid" id="invalid_email">Valid email is required<img src="img/invalid.png"></span>');
+                Color_border(true , email);
+                email.after(Message(text));
                 ;
             }
         } else {
-            email.css("border-bottom", "2px solid #c80000");
-            email.after('<span class="invalid" id="invalid_email">Valid email is required<img src="img/invalid.png"></span>');
+            Color_border(true , email);
+            email.after(Message(text));
             ;
         }
-    });
+    });*/;
     /*Поле PASSWORD*/
-    password.blur(function () {
-        $("#invalid_password").remove();
+    Proverca_poley(password,'Password is required',val_password);
+    /*password.blur(function () {
+        password.siblings().remove();
         if (password.val().length > 5) {
-            password.css("border-bottom", "2px solid #ccc");
+            Color_border(false ,password);
         } else {
-            password.css("border-bottom", "2px solid #c80000");
-            password.after('<span class="invalid" id="invalid_password">Password is required <img src="img/invalid.png"></span>');
+            proverca(true , email);
+            Color_border.after(Message(text));
         }
-    });
+    });*/;
     /*Поле SECONDPASSWORD*/
+
+    //Proverca_poley(second_password,'Passwords don\'t match',password.val());
+
     second_password.blur(function () {
-        $("#invalid_secondpassword").remove();
+        text='Passwords don\'t match';
+       second_password.siblings().remove();
         if (second_password.val() != '') {
             if (password.val() == second_password.val()) {
-                second_password.css("border-bottom", "2px solid #ccc");
+            //if (second_password.val().search(password.val())==0) {
+                Color_border(false , second_password);
             }
             else{
-                second_password.css("border-bottom", "2px solid #c80000");
-                second_password.after('<span class="invalid" id="invalid_secondpassword">Passwords don\'t match<img src="img/invalid.png"></span>');
+                Color_border(true , second_password);
+                second_password.after(Message(text));
             }
         } else {
-            second_password.css("border-bottom", "2px solid #c80000");
-            second_password.after('<span class="invalid" id="invalid_secondpassword">Passwords don\'t match<img src="img/invalid.png"></span>');
+            Color_border(true , second_password);
+            second_password.after(Message(text));
         }
     })
+    /*ЧЕКБОКС*/
+    checkbox.click(function() {
+        button.siblings('.invalid').remove();
+        if(!((checkbox).is(':checked'))){
+            button.after(Message('You mast accept terms and conditions'));
+        }
+    });
     /*КНОПКА*/
     form.submit(function (event) {
         mas.forEach(function (element) {
             element.blur();
         });
-
-        $("#invalid_checkbox").remove();
+        button.siblings('.invalid').remove();
         if (!($('input[name="checkbox-test"]').is(':checked'))) {
-            accept.after('<span class="invalid" id="invalid_checkbox">You mast accept terms and conditions<img src="img/invalid.png"></span>');
+            button.after(Message('You mast accept terms and conditions'));
         }
         if ($('.mistakes').find("span.invalid").length != 0) {
             event.preventDefault();
